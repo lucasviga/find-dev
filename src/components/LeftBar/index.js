8678bf585a320b32872a936d410818f142050233
Lucas Viga <lucasviga12@gmail.com>
2019-05-27 13:12:29 -0400

    list os usuarios cadastrados / conectado ao redux

--- src/components/LeftBar/index.js
+++ src/components/LeftBar/index.js
@@ -1,9 +1,33 @@
 import React, { Fragment } from "react";
 
-const LeftBar = () => (
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as UserActions from "../../store/actions/users";
+
+const LeftBar = props => (
   <Fragment>
     <h1>List of Users</h1>
+
+    <ul>
+      {props.users.data.map(user => (
+        <li key={user.id}>
+          <img src={user.avatar} alt={user.name} height={120} />
+          <p>{user.name}</p>
+          <p>{user.login}</p>
+        </li>
+      ))}
+    </ul>
   </Fragment>
 );
 
-export default LeftBar;
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(UserActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LeftBar);
