394c1fc84a349ce040131506b6164c0508a62d0b
Lucas Viga <lucasviga12@gmail.com>
2019-05-28 11:33:31 -0400

    correcao no import do ModalActions

--- src/components/AddUser/index.js
+++ src/components/AddUser/index.js
@@ -6,7 +6,11 @@ import Modal from "react-modal";
 //conecta o component com alguma info do redux
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as UserActions from "../../store/actions/users";
+
+import { Creators as ModalActions } from "../../store/ducks/modal";
+import { Creators as UsersActions } from "../../store/ducks/users";
+
+import "./styles.css";
 
 Modal.setAppElement(document.getElementById("root"));
 
@@ -30,41 +34,69 @@ class AddUser extends Component {
     userInput: ""
   };
 
-  handleAddUser = event => {
-    event.preventDefault();
-    this.props.addUserRequest(this.state.userInput);
+  handleFormSubmit = e => {
+    e.preventDefault();
+
+    const { loading } = this.props;
+
+    if (loading) return;
+
+    const { userInput } = this.state;
+
+    if (!userInput) return;
+
+    const {
+      addUserRequest,
+      modal: { cordinates }
+    } = this.props;
+
+    addUserRequest(userInput, cordinates);
     this.setState({ userInput: "" });
   };
 
-  handleFormSubmit = e => {};
-
-  handleHideModal = () => {};
+  handleHideModal = () => {
+    const { hideModal } = this.props;
+    hideModal();
+    this.setState({ userInput: "" });
+  };
 
   render() {
+    const { modal } = this.props;
+    const { userInput } = this.state;
     return (
-      <div>
-        <form onSubmit={this.handleAddUser}>
+      <Modal
+        isOpen={modal.visible}
+        onRequestClose={this.handleHideModal}
+        contentLabel="Add user Modal"
+        className="modal-container"
+        overlayClassName="modal-overlay"
+      >
+        <h2>Adicionar novo dev</h2>
+
+        <form onSubmit={this.handleAddUser} className="form">
           <input
             placeholder="user do github"
-            value={this.state.userInput}
+            value={userInput}
             onChange={e => this.setState({ userInput: e.target.value })}
           />
-          <button type="submit">Adicionar</button>
-          {this.props.users.loading && <span>carregando...</span>}
+          <div className="container-button">
+            <button type="button" onClick={this.handleHideModal}>
+              Cancelar
+            </button>
+            <button type="submit">Adicionar</button>
+          </div>
         </form>
-
-        {this.props.users.error && <span>{this.props.users.error}</span>}
-      </div>
+      </Modal>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  users: state.users
+  modal: state.modal
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(UserActions, dispatch);
+  bindActionCreators({ ...ModalActions, ...UsersActions }, dispatch);
 
 export default connect(
   mapStateToProps,
