2ae0fc1fdc075d4235b40bb67455333d5f32a0ac
Lucas Viga <lucasviga12@gmail.com>
2019-05-28 14:24:53 -0400

    importacao do componente Map

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,10 +3,11 @@ import React, { Fragment } from "react";
 //components
 import AddUser from "../../components/AddUser/";
 import LeftBar from "../../components/LeftBar/";
-// import Map from "../../components/Map/";
+import Map from "../../components/Map/";
 
 const Main = () => (
   <Fragment>
+    <Map />
     <LeftBar />
     <AddUser />
   </Fragment>
