e9adc6cc1588f1555370d21afba869a824655f9e
Lucas Viga <lucasviga12@gmail.com>
2019-05-29 09:46:34 -0400

    verifica se é o user é duplicado; mostra notificação ao usuario

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import store from "./store";
 // import Map from "./components/Map";
 import Routes from "./routes";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "font-awesome/css/font-awesome.css";
 import "./styles.css";
 
 const App = () => (
   <Provider store={store}>
     <Routes />
+    <ToastContainer autoclose={3000} />
   </Provider>
 );
 export default App;
--- src/store/sagas/users.js
+++ src/store/sagas/users.js
@@ -1,4 +1,5 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
+import { toast } from "react-toastify";
 import api from "../../services/api";
 
 import { Creators as UsersActions } from "../ducks/users";
@@ -8,17 +9,38 @@ export function* addUser(action) {
   try {
     const { data } = yield call(api.get, `/${action.payload.user}`);
 
-    const userData = {
-      id: data.id,
-      name: data.name,
-      login: data.login,
-      avatar: data.avatar_url,
-      cordinates: action.payload.cordinates
-    };
+    /**
+     * verica meu state que é chamado de users -> data (o nome do meu state) -> find (metodo para encontrar)
+     * faz a comparacao do meu user.id (do meu state) === com o data.id vindo da requisicao (api)
+     * */
+    const isDuplicated = yield select(state =>
+      state.users.data.find(user => user.id === data.id)
+    );
 
-    yield put(UsersActions.addUserSuccess(userData));
+    if (isDuplicated) {
+      yield put(UsersActions.addUserFailure("Usuário já existe"));
+      toast.warn("Usuário já existe", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    } else {
+      const userData = {
+        id: data.id,
+        name: data.name,
+        login: data.login,
+        avatar: data.avatar_url,
+        cordinates: action.payload.cordinates
+      };
+
+      yield put(UsersActions.addUserSuccess(userData));
+      toast.success("Adicionado com sucesso!", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    }
   } catch (err) {
-    yield put(UsersActions.addUserFailure("erro ao adicionar o usuário"));
+    yield put(UsersActions.addUserFailure("Erro ao adicionar o usuário"));
+    toast.warn("Erro ao adicionar o usuário", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
   } finally {
     yield put(ModalActions.hideModal());
   }
